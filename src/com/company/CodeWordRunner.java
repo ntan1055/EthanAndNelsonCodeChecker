29741ed10cd4ebc4220d581333e7846c59c79425
ItsMeEthan <43207586+ItsMeEthan@users.noreply.github.com>
2018-11-28 08:40:53 -0500

    Update CodeWordRunner.java

--- src/com/company/CodeWordRunner.java
+++ src/com/company/CodeWordRunner.java
@@ -4,5 +4,12 @@ public class CodeWordRunner
 {
     public static void main(String[] args) {
 	// write your code here
+        StringChecker sc1= new CodeWordChecker(5,6, "$");
+        StringChecker sc2= new CodeWordChecker(6,7,"pass");
+
+        System.out.println(sc1.isValid("happy"));
+        System.out.println(sc1.isValid("happy$"));
+        System.out.println(sc1.isValid("Code"));
+        System.out.println(sc1.isValid("happyCode"));
     }
 }
